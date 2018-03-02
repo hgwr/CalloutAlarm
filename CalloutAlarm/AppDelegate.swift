b7c700d0493f866556b397cb14e829bac6da6459
Shigeru Hagiwara <hgwrsgr@gmail.com>
2018-03-02 09:34:45 +0900

    Initial Commit

---
+++ CalloutAlarm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CalloutAlarm
+//
+//  Created by Shigeru Hagiwara on 2018/03/02.
+//  Copyright © 2018年 Shigeru Hagiwara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CalloutAlarm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CalloutAlarm
+//
+//  Created by Shigeru Hagiwara on 2018/03/02.
+//  Copyright © 2018年 Shigeru Hagiwara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
