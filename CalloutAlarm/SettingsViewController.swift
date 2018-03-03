e232d1a4449a921ba8d99d6090eb68acfe962121
Shigeru Hagiwara <hgwrsgr@gmail.com>
2018-03-03 13:33:51 +0900

    settings view controller

---
+++ CalloutAlarm/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  CalloutAlarm
+//
+//  Created by Shigeru Hagiwara on 2018/03/03.
+//  Copyright © 2018年 Shigeru Hagiwara. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
