032a62c2780d782c5bdceb6918114c2d3fd0bbe9
Shigeru Hagiwara <hgwrsgr@gmail.com>
2018-03-05 06:33:44 +0900

    設定画面にアウトレット追加

--- CalloutAlarm/SettingsViewController.swift
+++ CalloutAlarm/SettingsViewController.swift
@@ -9,10 +9,20 @@
 import Cocoa
 
 class SettingsViewController: NSViewController {
-
+    @IBOutlet weak var startTimeStrField: NSTextField!
+    @IBOutlet weak var finishTimeStrField: NSTextField!
+    @IBOutlet weak var speechTextAtTheStartField: NSTextField!
+    @IBOutlet weak var timeSpeechFormatField: NSTextField!
+    @IBOutlet weak var volumeSlider: NSSlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+    @IBAction func timeSpeechTestClicked(_ sender: Any) {
+    }
+  
+    @IBAction func startSpeechTestClicked(_ sender: Any) {
+    }
 }
